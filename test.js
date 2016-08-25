f786791950774337e85239dd078fa4b12ceda04e
zgy03916618421 <576428698@qq.com>
2016-08-25 15:53:55 +0800

    zhixing

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 var w = 720;
 var h = 840;
 var canvas = new Canvas(w,h);
+var buffer = new Buffer('知性');
+console.log(buffer);
 var context = canvas.getContext('2d');
 context.font = '50px PingFangSC-Regular';
 context.fillText('知性', 100, 200);
