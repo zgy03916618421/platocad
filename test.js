ef0ddc2f99451ecd7d9b072ba478e64436264664
zgy03916618421 <576428698@qq.com>
2016-08-25 19:35:58 +0800

    msyh

--- test.js
+++ test.js
@@ -6,11 +6,18 @@ var path = require('path');
 var fs = require('fs');
 var w = 720;
 var h = 840;
+var Font = Canvas.Font;
+function fontFile(name) {
+    return path.join(__dirname,'/font/',name);
+}
+var msyhFont = new Font('msyhFont',fontFile('msyh.ttf'));
+msyhFont.addFace(fontFile('msyhbd.tff'),'bold');
 var canvas = new Canvas(w,h);
 var buffer = new Buffer('知性');
 console.log(buffer);
 var context = canvas.getContext('2d');
-context.font = '50px PingFangSC-Regular';
+context.addFont(msyhFont);
+context.font = '50px msyhFont';
 context.fillText('知性', 100, 200);
 var buff = canvas.toBuffer();
 fs.writeFileSync('test1.png',buff);
